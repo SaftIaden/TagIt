0c1e125876c060ecb92422d638f580a79543877d
Daniel Diewald <diewald.d02@htlwienwest.at>
2023-03-13 19:25:07 +0100

    test data in gallery

--- client/src/views/GalleryView.vue
+++ client/src/views/GalleryView.vue
@@ -14,10 +14,9 @@
       <template v-slot:hint> search by title </template>
     </q-input>
   </div>
-  <div class="container row" style="padding: 1rem">
+  <div class="container row justify-center" style="padding: 1rem">
     <q-card
-      class="my-card col"
-      style="max-width: 25rem; widht: 90vw"
+      class="my-card col col-md-4 col-sm-12 col-xs-12 q-ma-sm"
       v-for="post in filteredList"
       :key="post.id"
     >
@@ -48,7 +47,7 @@
   </div>
 
   <q-dialog v-model="alert">
-    <q-card>
+    <q-card class="my-card">
       <q-card-section>
         <div class="text-h6">{{ selected }}</div>
       </q-card-section>
@@ -67,23 +66,91 @@
 .my-card {
   overflow: hidden;
   border-radius: 15px;
+  max-width: 30rem;
+  width: 90vw;
 }
 </style>
 <script>
 import { ref } from 'vue';
 
+class Post {
+  constructor(title, link, author, img) {
+    this.title = title;
+    this.link = link;
+    this.description = author;
+    this.img = img;
+  }
+}
+
 export default {
   setup() {
     return {
+      alert: ref(false),
       search: ref(''),
       posts: {},
       selected: ref(''),
       selectedde: ref(''),
+      postList: [
+        new Post(
+          'Vue.js',
+          'https://vuejs.org/',
+          'Chris',
+          'https://vuejs.org//images/logo.png'
+        ),
+        new Post(
+          'React.js',
+          'https://facebook.github.io/react/',
+          'Tim',
+          'https://daynin.github.io/clojurescript-presentation/img/react-logo.png'
+        ),
+        new Post(
+          'Angular.js',
+          'https://angularjs.org/',
+          'Sam',
+          'https://angularjs.org/img/ng-logo.png'
+        ),
+        new Post(
+          'Ember.js',
+          'http://emberjs.com/',
+          'Rachel',
+          'http://www.gravatar.com/avatar/0cf15665a9146ba852bf042b0652780a?s=200'
+        ),
+        new Post(
+          'Meteor.js',
+          'https://www.meteor.com/',
+          'Chris',
+          'http://hacktivist.in/introduction-to-nodejs-mongodb-meteor/img/meteor.png'
+        ),
+        new Post(
+          'Aurelia',
+          'http://aurelia.io/',
+          'Tim',
+          'https://cdn.auth0.com/blog/aurelia-logo.png'
+        ),
+        new Post(
+          'Node.js',
+          'https://nodejs.org/en/',
+          'A. A. Ron',
+          'https://code-maven.com/img/node.png'
+        ),
+        new Post(
+          'Pusher',
+          'https://pusher.com/',
+          'Alex',
+          'https://avatars1.githubusercontent.com/u/739550?v=3&s=400'
+        ),
+        new Post(
+          'Feathers.js',
+          'http://feathersjs.com/',
+          'Chuck',
+          'https://cdn.worldvectorlogo.com/logos/feathersjs.svg'
+        ),
+      ],
     };
   },
   computed: {
     filteredList() {
-      return this.posts.filter((post) => {
+      return this.postList.filter((post) => {
         return post.title.toLowerCase().includes(this.search.toLowerCase());
       });
     },
